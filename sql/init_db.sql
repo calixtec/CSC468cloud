da8f2d949b6476b189939f841654eacd48f39427
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-13 09:26:28 -0400

    Delete sql directory

--- sql/init_db.sql
+++
@@ -1,5 +0,0 @@
-Create database csc468;
-Use csc468;
-Create table if not exist cattle
-(name VARCHAR(255), breed VARCHAR(255), color VARCHAR(255));
-LOAD data INFILE var/lib/mysql-files/ani.csv into table cattle;
