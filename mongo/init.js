7a90defd4d7296925a683d00aa70f9cef7406710
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-31 14:56:15 -0400

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -14,7 +14,8 @@
 
 // var result = inventoryDB.items.insertMany(products);
 //-------
-
+snippet install mongocompat
+snippet run initialize_inventory
 var inventoryDB = db.getSiblingDB('inventory');
 inventoryDB.createCollection('items');
 
