b3c189ec83a2d6f58e775d7f47171a19f575ed77
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-31 07:33:03 -0400

    Update entrypoint.sh

--- mongo/entrypoint.sh
+++ mongo/entrypoint.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
 
-mongosh --username mongoadmin --password secret --authenticationDatabase admin /docker-entrypoint-initdb.d/init.js
-
-
-mongod --auth &
-
-sleep 10
-
-
-mongoimport --db inventory --collection items --file /usr/src/app/data/products.json
-exec "$@"
