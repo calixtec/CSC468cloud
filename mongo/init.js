f21229a4d9b37cb7b977333a0039235301007afb
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-31 13:46:36 -0400

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -1,15 +1,43 @@
+// var inventoryDB = db.getSiblingDB('inventory');
+// inventoryDB.createCollection('items');
+
+// inventoryDB.createUser({
+//     user: "mongoadmin2",
+//     pwd: "password",
+//     roles: [{ role: "readWrite", db: "inventory" }]
+// });
+// var products = [
+//     { product: "headphones", price: 20, date: "March 30, 2024" },
+//     { product: "macbook", price: 1000, date: "March 30, 2024" },
+//     { product: "mouses", price: 15, date: "March 30, 2024" }
+// ];
+
+// var result = db.items.insertMany(products);
+
+
 var inventoryDB = db.getSiblingDB('inventory');
-inventoryDB.createCollection('items');
 
-inventoryDB.createUser({
-    user: "mongoadmin2",
-    pwd: "password",
-    roles: [{ role: "readWrite", db: "inventory" }]
-});
-var products = [
-    { product: "headphones", price: 20, date: "March 30, 2024" },
-    { product: "macbook", price: 1000, date: "March 30, 2024" },
-    { product: "mouses", price: 15, date: "March 30, 2024" }
-];
+// Check if the collection exists
+if (!inventoryDB.items) {
+    inventoryDB.createCollection('items');
+}
 
-var result = db.items.insertMany(products);
+// Check if the collection is empty
+if (inventoryDB.items.count() === 0) {
+    inventoryDB.createUser({
+        user: "mongoadmin2",
+        pwd: "password",
+        roles: [{ role: "readWrite", db: "inventory" }]
+    });
+
+    var products = [
+        { product: "headphones", price: 20, date: "March 30, 2024" },
+        { product: "macbook", price: 1000, date: "March 30, 2024" },
+        { product: "mouses", price: 15, date: "March 30, 2024" }
+    ];
+
+    var result = db.items.insertMany(products);
+    print("Data inserted successfully.");
+} else {
+    print("Collection 'items' is not empty. Data insertion skipped.");
+}
