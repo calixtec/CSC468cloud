6fbd3078d45f7559b6b245bcd691c0601deb7553
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-31 14:08:42 -0400

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -1,3 +1,20 @@
+// var inventoryDB = db.getSiblingDB('inventory');
+// inventoryDB.createCollection('items');
+
+// inventoryDB.createUser({
+//     user: "mongoadmin2",
+//     pwd: "password",
+//     roles: [{ role: "readWrite", db: "inventory" }]
+// });
+// var products = [
+//     { product: "headphones", price: 20, date: "March 30, 2024" },
+//     { product: "macbook", price: 1000, date: "March 30, 2024" },
+//     { product: "mouses", price: 15, date: "March 30, 2024" }
+// ];
+
+// var result = inventoryDB.items.insertMany(products);
+//-------
+
 var inventoryDB = db.getSiblingDB('inventory');
 inventoryDB.createCollection('items');
 
@@ -6,11 +23,6 @@ inventoryDB.createUser({
     pwd: "password",
     roles: [{ role: "readWrite", db: "inventory" }]
 });
-var products = [
-    { product: "headphones", price: 20, date: "March 30, 2024" },
-    { product: "macbook", price: 1000, date: "March 30, 2024" },
-    { product: "mouses", price: 15, date: "March 30, 2024" }
-];
-
-var result = inventoryDB.items.insertMany(products);
-
+var productsJsonFilePath = "/usr/src/app/data/products.json";
+var jsonData = load(productsJsonFilePath);
+var result =  inventoryDB.items.insertMany(jsonData);
