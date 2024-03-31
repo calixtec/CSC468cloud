3296cc274f8ce3e80b517191da01cb69ee9c654d
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-31 10:16:55 -0400

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -1,11 +1,12 @@
+db = db.getSiblingDB('inventory');
+db.createCollection('items');
+
 db.createUser({
     user: "mongoadmin",
     pwd: "secret",
     pwd: "password",
-    roles: [{ role: "root", db: "admin" }]
+    roles: [{ role: "root", db: "admin" },{ role: "root", db: "inventory" }]
 });
 
-db = db.getSiblingDB('inventory');
-db.createCollection('items');
 
 
