6338ebb3d4495eb0072a003284f7a573c7b5fe63
calixtec <90633174+calixtec@users.noreply.github.com>
2024-03-31 10:46:45 -0400

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -1,7 +1,6 @@
 db.createUser({
     user: "mongoadmin",
     pwd: "secret",
-    pwd: "password",
     roles: [
         { role: "root", db: "admin" },
         { role: "dbOwner", db: "inventory" }
