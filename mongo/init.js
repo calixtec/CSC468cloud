7479497ae5bc29cb46ee36500419e0d272ecd8fd
calixtec <90633174+calixtec@users.noreply.github.com>
2024-04-10 17:12:25 -0400

    Update init.js

--- mongo/init.js
+++ mongo/init.js
@@ -21,7 +21,7 @@ inventoryDB.createCollection('items');
 inventoryDB.createUser({
     user: "mongoadmin2",
     pwd: "password",
-    roles: [{ role: "readWrite", db: "inventory" }],,
+    roles: [{ role: "readWrite", db: "inventory" }],
   mechanisms: [ 'SCRAM-SHA-1', 'SCRAM-SHA-256' ]
 });
 var productsJsonFilePath = "/usr/src/app/data/products.json";
